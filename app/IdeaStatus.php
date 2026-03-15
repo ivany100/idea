5d781d73bc710b6e970ddf3d20e17992c1b68626
Ivan Yurchak <ivanyurchak3@gmail.com>
2026-03-15 15:24:54 +0100

    Add formating

--- app/IdeaStatus.php
+++ app/IdeaStatus.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App;
 
 enum IdeaStatus: string
@@ -8,7 +10,8 @@ enum IdeaStatus: string
     case IN_PROGRESS = 'in_progress';
     case COMPLETED = 'completed';
 
-    public function label(): string {
+    public function label(): string
+    {
         return match ($this) {
             self::PENDING => 'Pending',
             self::IN_PROGRESS => 'In Progress',
