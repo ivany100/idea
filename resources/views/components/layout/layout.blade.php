b175abdfd141586f907af4bed64ccadae2da3606
Ivan Yurchak <ivanyurchak3@gmail.com>
2026-03-16 21:14:00 +0100

    Add log in flash message with Alpine

--- resources/views/components/layout/layout.blade.php
+++ resources/views/components/layout/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Idea</title>
-    @vite(['resources/css/app.css'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="bg-background text-foreground">
     <x-layout.nav />
@@ -12,5 +12,16 @@
     <main class="max-w-7xl mx-auto px-6">
         {{ $slot }}
     </main>
+
+    @session('success')
+        <div
+            x-data="{ show: true }"
+            x-init="setTimeout(() => show = false, 3000)"
+            x-show="show"
+            class="bg-primary px-4 py-3 absolute bottom-4 right-4 rounded-lg"
+        >
+            {{ $value }}
+        </div>
+    @endsession
 </body>
 </html>
